1c11994de337868061010e9dc58c37f0f499e6f4
bobdb <bobdb40@gmail.com>
2024-10-18 18:04:03 -0400

    init

---
+++ src/main/java/net/bobdb/fun_with_chatbots/FunWithChatbotsApplication.java
@@ -0,0 +1,13 @@
+package net.bobdb.fun_with_chatbots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunWithChatbotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunWithChatbotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/bobdb/fun_with_chatbots/FunWithChatbotsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bobdb.fun_with_chatbots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunWithChatbotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
