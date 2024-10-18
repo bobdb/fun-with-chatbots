42ba02c04735e0a318db3e64a239ede2a1df1633
bobdb <bobdb40@gmail.com>
2024-10-18 19:39:52 -0400

    added Tailwind/HTMX frontend and updated chat POST

--- src/main/java/net/bobdb/fun_with_chatbots/ChatController.java
+++ src/main/java/net/bobdb/fun_with_chatbots/ChatController.java
@@ -3,6 +3,7 @@ package net.bobdb.fun_with_chatbots;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,9 +21,9 @@ class ChatController {
     }
 
     @PostMapping("/chat")
-    String chat() {
+    String chat(@RequestParam String message) {
         return chatClient.prompt()
-                .user("Tell me a math fact.")
+                .user(message)
                 .call()
                 .content();
     }
